a519b6242f8ded8d1f196f840598355af7e2f660
parlyresk <parlyresk325@gmail.com>
2024-10-04 16:44:11 +0900

    pbs-288--feat: voc annotation

--- src/main/java/com/pobluesky/voc/VocApplication.java
+++ src/main/java/com/pobluesky/voc/VocApplication.java
@@ -4,10 +4,12 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableCaching
 @SpringBootApplication
 @ComponentScan(basePackages = {
     "com.pobluesky.voc.global",
